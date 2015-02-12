a90e0751153d09e9eef852f05b32e860f751d063
Jason Dubaniewicz <jason.dubaniewicz@bigcommerce.com>
2015-02-12 08:20:01 -0800

    editMode first attempt

--- js/controllers/todoCtrl.js
+++ js/controllers/todoCtrl.js
@@ -21,16 +21,32 @@ mitch.controller('ListCtrl', function ListCtrl($scope, $location) {
 				{ id: 43, content: "anchovies" },
 				{ id: 44, content: "mushrooms" }
 			]
-		};
+		},
+		items,
+		title;
 
 	$scope.list = angular.copy(backup);
+	items = $scope.list.items;
+	title = $scope.list.title;
 
-	// get rid of the annoying #
+	$scope.newItem = '';
+	$scope.editedItem = null;
+	$scope.editMode = false;
+
+	// get rid of the annoying #, let's use HTML5 routing so we can have canonical URLs that get remembered
 	if ($location.path() === '') {
 		$location.path('/');
 	}
 
 	$scope.location = $location;
+
+	$scope.handleButton = function() {
+		if ($scope.editMode) {
+			$scope.editMode = false;
+		} else {
+			$scope.logList();
+		}
+	}
 
 	/**
 	 * when the user clicks 'Vote' simply log the list object for now
@@ -43,6 +59,63 @@ mitch.controller('ListCtrl', function ListCtrl($scope, $location) {
 	};
 
 	$scope.toBackup = function() {
+		$scope.editMode = false;
 		$scope.list = angular.copy(backup);
+	};
+
+	$scope.addItem = function () {
+		var newItem = $scope.newItem.trim(),
+			newId = 0;//Math.max.apply(null, list.items.map(function(e){ return e.id })) + 1;
+
+		if (newItem.length === 0) {
+			return;
+		}
+
+		items.push({
+			id: newId,
+			content: newItem
+		});
+
+		$scope.newItem = '';
+	};
+
+	$scope.editItem = function (item) {
+		$scope.editedItem = item;
+		// Clone the original item to restore it on demand.
+		$scope.originalItem = angular.extend({}, item);
+	};
+
+	$scope.doneEditing = function (item) {
+		// if we double clicked on a different item, don't set editedItem to null
+		if (item.content === $scope.editedItem.content) {
+			$scope.editedItem = null;
+			item.content = item.content.trim();
+
+			if (!item.content) {
+				$scope.removeItem(item);
+			}
+		}
+	};
+
+	$scope.enterEditMode = function() {
+		title = $scope.list.title;
+		$scope.editedItem = null;
+		$scope.editMode = true;
 	}
+
+	$scope.revertEditing = function (item) {
+		var idx = items.indexOf(item);
+
+		items[idx] = $scope.originalItem;
+		$scope.doneEditing($scope.originalItem);
+	};
+
+	$scope.revertTitle = function() {
+		$scope.list.title = title;
+		$scope.editMode = false;
+	}
+
+	$scope.removeItem = function (item) {
+		items.splice(items.indexOf(item), 1);
+	};
 });
